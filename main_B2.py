c1c98e1ec6d2005a0d37551c300e7c209038f91b
MeRIC-Lab <100504339+MeRIC-Lab@users.noreply.github.com>
2022-04-13 10:51:24 +0900

    Add files via upload

---
+++ main_B2.py
@@ -0,0 +1,9 @@
+import module_for_B
+
+cal1 = module_for_B.Calculator()
+
+print(cal1.add(2))
+print(cal1.add(3))
+print(module_for_B.add(2,5))
+print(module_for_B.sub(2,5))
+
---
+++ main_B3.py
@@ -0,0 +1,9 @@
+import module_for_B as md
+
+cal1 = md.Calculator()
+
+print(cal1.add(2))
+print(cal1.add(3))
+print(md.add(2,5))
+print(md.sub(2,5))
+
